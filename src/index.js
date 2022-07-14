13f8a1bb59e638a46fc9be3f66355387da51b9f4
Alex Edwards <alextcedwards@gmail.com>
2022-07-14 13:26:30 -0600

    commit on master

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-app.use(express.static(__dirname));
 
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
 
 ReactDOM.render(
   <React.StrictMode>
